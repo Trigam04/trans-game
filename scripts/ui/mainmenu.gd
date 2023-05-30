725fe87f81e74563b70a8586d62973e0cab49521
Mae <stringy@mae.wtf>
2023-05-30 01:40:50 +0100

    Implement basic main menu

---
+++ scripts/ui/mainmenu.gd
@@ -0,0 +1,17 @@
+extends Node
+
+# TODO: implement these
+
+func _on_continue():
+  pass
+
+func _on_new_game():
+  pass
+
+func _on_options():
+  # append options scene
+  pass
+
+func _on_quit():
+  # goodbye!
+  get_tree().quit(0)
